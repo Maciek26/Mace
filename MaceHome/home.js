5d3ae327386c14b9f9891d88d6ef8c2e9021a8e5
maciek.kierszka <maciek.kierszka@ict-scouts.ch>
2022-05-14 12:35:11 +0200

    home upgrade

--- MaceHome/home.js
+++ MaceHome/home.js
@@ -1,20 +1,20 @@
-var test = document.getElementsByClassName("svg1");
-    var stroke = document.getElementsByClassName("rect10");
-
-    console.log(test);
-    console.log(test[0]);
-    console.log(stroke);
-    console.log(stroke[0]);
-
-    setInterval(function () {
-        stroke.style.display = "flex";
-        stroke.style.animation = "blink 5s";
-        var Interval = setInterval(function () {
-            stroke.style.animation = "blink2 5s";
-            var Interval2 = setInterval(function () {
-                stroke.style.display = "none";
-                clearInterval(Interval2);
-            }, 5000)
-            clearInterval(Interval);
-        }, 7000)
-    }, 1000);
+// var test = document.getElementsByClassName("svg1");
+//     var stroke = document.getElementsByClassName("rect10");
+//
+//     console.log(test);
+//     console.log(test[0]);
+//     console.log(stroke);
+//     console.log(stroke[0]);
+//
+//     setInterval(function () {
+//         stroke.style.display = "flex";
+//         stroke.style.animation = "blink 5s";
+//         var Interval = setInterval(function () {
+//             stroke.style.animation = "blink2 5s";
+//             var Interval2 = setInterval(function () {
+//                 stroke.style.display = "none";
+//                 clearInterval(Interval2);
+//             }, 5000)
+//             clearInterval(Interval);
+//         }, 7000)
+//     }, 1000);
